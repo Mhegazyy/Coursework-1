00a5425ffde36e7d5563e46828ef68abc75d7a3f
Mhegazyy <mh2101083@tkh.edu.eg>
2022-10-28 03:18:06 +0200

    Update driver.py

--- driver.py
+++ driver.py
@@ -10,16 +10,16 @@ file.close()
 
 file1 = open("message.txt","r+")
 holder = file1.read()
-tt = encrypt.encrypt(holder,key)
+txxt = encrypt.encrypt(holder,key)
 
 file2 = open("Encrypted Text.txt","w")
-file2.writelines(tt)
+file2.writelines(txxt)
 file2.close()
 
 file1 = open("Encrypted Text.txt","r+")
 holder = file1.read()
-tt = decrypt.decrypt(holder,key)
+txxt = decrypt.decrypt(holder,key)
 
 file2 = open("Decrypted Text.txt","w")
-file2.writelines(tt)
+file2.writelines(txxt)
 file2.close()
