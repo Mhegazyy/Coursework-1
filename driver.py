1202febbc3542a1e3d139b1470183244bf49915f
Mhegazyy <mh2101083@tkh.edu.eg>
2022-11-09 20:24:15 +0200

    Finalizing the code for marking

--- driver.py
+++ driver.py
@@ -8,18 +8,22 @@ file = open("Key.txt","r+")
 key = file.read()
 file.close()
 
+#Opening the Message file
 file1 = open("Message.txt","r+")
 holder = file1.read()
 txxt = encrypt.encrypt(holder,key)
 
+#Writing the encrypted text
 file2 = open("Encrypted Text.txt","w")
 file2.write(txxt)
 file2.close()
 
+#Pulling the encrypted text from the file for decryption
 file1 = open("Encrypted Text.txt","r+")
 holder = file1.read()
 txxt = decrypt.decrypt(holder,key)
 
+#Writing the decrypted text
 file2 = open("Decrypted Text.txt","w")
 file2.write(txxt)
 file2.close()
